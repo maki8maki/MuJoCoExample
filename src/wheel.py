3598ff2d254889a716a6edef810cba4cfa719027
maki8maki <maki8maki.tech@gmail.com>
2025-05-28 21:42:45 +0900

    差動駆動型ロボットのGUIを実装

---
+++ src/wheel.py
@@ -0,0 +1,48 @@
+import glfw
+
+from viewer import Viewer
+
+MJCF_PATH = "assets/wheel.xml"
+
+CAMERA_CONFIG = {
+    "distance": 5.0,
+    "azimuth": 180.0,
+    "elevation": -30.0,
+    "lookat": [0, 0.0, 0.75],
+}
+
+
+class WheelViewer(Viewer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(mjcf_path=MJCF_PATH, camera_config=CAMERA_CONFIG, title="Wheel", *args, **kwargs)
+
+    def _key_callback(self, window, key, scancode, action, mods):
+        if action in [glfw.PRESS, glfw.REPEAT]:
+            FORCE = 1.0
+        else:
+            FORCE = 0.0
+        if key == glfw.KEY_UP:
+            self.data.ctrl[0] = FORCE
+            self.data.ctrl[1] = FORCE
+        elif key == glfw.KEY_DOWN:
+            self.data.ctrl[0] = -FORCE
+            self.data.ctrl[1] = -FORCE
+        elif key == glfw.KEY_LEFT:
+            self.data.ctrl[0] = -FORCE
+            self.data.ctrl[1] = FORCE
+        elif key == glfw.KEY_RIGHT:
+            self.data.ctrl[0] = FORCE
+            self.data.ctrl[1] = -FORCE
+        else:
+            super()._key_callback(window, key, scancode, action, mods)
+
+    def _create_overlay(self):
+        super()._create_overlay()
+
+        self.add_overlay(self.TOPLEFT, "Forward ([Up]) / Backword ([Down])", "")
+        self.add_overlay(self.TOPLEFT, "[Left] / [Right] Rotation", "")
+
+
+if __name__ == "__main__":
+    viewer = WheelViewer()
+    viewer()
